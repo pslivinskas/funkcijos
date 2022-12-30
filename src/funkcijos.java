bc64e8ca02a1244c357497e27434f250bc9c834b
pslivinskas <povilaslsivinskas@gmail.com>
2022-12-30 16:56:11 +0200

    Initial commit

---
+++ src/funkcijos.java
@@ -0,0 +1,138 @@
+import java.util.Arrays;
+
+public class funkcijos {
+//              public static void main(String[] args) {
+//                int[] nums = rndIntArr(60,-160,200);
+//                printIntArr(nums);
+//                nums = rndIntArr(60,-160,200);
+//                printIntArr(nums);
+//                nums = rndIntArr(60,-160,200);
+//                printIntArr(nums);
+//                nums = rndIntArr(60,-160,200);
+//                printIntArr(nums);
+//
+//                nums = rndIntArr(60,-160,200);
+//                printIntArr(nums);
+//                nums = rndIntArr(60,-160,200);
+//                printIntArr(nums);
+//                nums = rndIntArr(60,-160,200);
+//                printIntArr(nums);
+//            }
+//
+//            public static void printIntArr(int[] arr){
+//                String res = "";
+//                for (int i = 0; i < arr.length; i++) {
+//                    res += arr[i] + ", ";
+//                }
+//                System.out.println( res.substring(0,res.length() -2) + "." );
+//            }
+//
+//            public static int[] rndIntArr(int length, int min, int max){
+//                int[] nums = new int[length];
+//                for (int i = 0; i < length; i++) {
+//                    nums[i] = (int) (min + Math.round(Math.random() * (max - min)));
+//                }
+//                return nums;
+
+    //////// 1
+//    public static void main(String[] args) {
+//        System.out.println(sumInt(16, 30));
+//        sayHi();
+//        int sum = sumInt(40,80);
+//        double rnd = Math.random();
+//    }
+//    public static void sayHi(){
+//        System.out.println("labas rytas =)");
+//    }
+//    public static int sumInt ( int a, int b){
+//        return a + b;
+
+    /////////// 2
+//    public static void main(String[] args) {
+//        System.out.println(PISq() * PISq());
+//    }
+//    public static double PISq(){
+//        return 3.14;
+//    }
+//}
+/////////// 3
+//    public static void main(String[] args) {
+//        naujas(3, 6);
+//    }
+//    public static void naujas(int kint1, int kint2) {
+//        System.out.println(kint1 * kint2);
+//    }
+//}
+
+///////////4
+//public static void main(String[] args) {
+//    int[] masyvas = {5, 8, 3};
+//    nums(masyvas);
+//}
+//
+//    public static void nums(int[] arr) {
+//    for (int i = 0; i < arr.length; i++) {
+//        System.out.println(arr[1]);
+//    }
+//
+//
+//
+//}
+//}
+
+
+//    5, 6, 7 uzduotys
+
+
+//    public static void main(String[] args) {
+//
+//
+//        int[] masyvas3 = rndArr();
+//        for (int i = 0; i < masyvas3.length; i++) {
+//            System.out.println(masyvas3[i]);
+//        }
+//
+//        int res = arrSum(masyvas3);
+//        System.out.println(res);
+//
+//        double vidurkis = arrAvg(masyvas3);
+//        System.out.println(vidurkis);
+//    }
+//
+//
+//    public static int[] rndArr() {
+//        int[] arr = new int[5];
+//        for (int i = 0; i < 5; i++) {
+//            arr[i] = (int) Math.round(Math.random() * 100);
+//
+//        }
+//        return arr;
+//
+//
+//    }
+//
+//    public static int arrSum(int[] arr) {
+//        int sum = 0;
+//        for (int i = 0; i < arr.length; i++) {
+//            sum += arr[i];
+//        }
+//        return sum;
+//    }
+//
+//    public static double arrAvg(int[] arr) {
+//        double avg = 0;
+//        for (int i = 0; i < arr.length; i++) {
+//            avg += arr[i];
+//
+//        }
+//        return avg  / arr.length;
+//    }
+//
+//}
+
+    public static void main(String[] args) {
+
+
+
+
+    }
